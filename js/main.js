b746337c1e8cee6f66a3a5d8f0e8783f5f78dca8
efriis <e@efriis.com>
2014-11-04 14:19:48 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-function test() {
-  console.log("hello world");
-}
-test();
+ window.onload = function() {
+
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+
+    function preload () {
+
+        game.load.image('logo', 'phaser.png');
+
+    }
+
+    function create () {
+
+        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+
+    }
+
+};
