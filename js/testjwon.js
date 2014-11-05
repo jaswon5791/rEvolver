a2a7a5b9f34114170e62c4fae9bdeab5fff8aa25
Jason W <jasonwon628@gmail.com>
2014-11-05 00:13:41 -0500

    js work

--- js/testjwon.js
+++ js/testjwon.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
 function preload () {
     game.stage.disableVisibilityChange = true;
@@ -7,17 +7,36 @@ function preload () {
 
 }
 
+var logo;
+
 function create () {
 
-    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
+    ground = game.add.group();
+    for (var i = 0 ; i < 10 ; i++) {
+        var rect = game.add.sprite(i*game.world.width/11, game.world.height/2, null);
+        game.physics.enable(rect, Phaser.Physics.ARCADE);
+        rect.body.setSize(50, 5, 0, 0); // set the size of the rectangle
+        rect.body.immovable = true;
+        ground.add(rect);
+    }
+
+    /*
+    logo = game.add.sprite(game.world.centerX, 0, 'logo');
+    //logo.anchor.setTo(0.5, 0.5);
     game.physics.arcade.enable(logo);
     logo.body.bounce.y = .9;
     logo.body.gravity.y = 400;
     logo.body.collideWorldBounds = true;
+    */
 
 }
 
 function update () {
+    logo.angle += 1;
+}
+
+function render () {
+
+    game.debug.renderRectangle(floor,'#0fffff');
 
 }
